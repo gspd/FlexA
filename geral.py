b4aa2195f1640b5311b41023e1384e0f982c0088
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2013-09-25 18:54:18 -0300

    Criado classe para envio de mensagens

---
+++ geral.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import os
+import socket
+from threading import Thread
+
+class EnviaMensagem(Thread):
+    """Classe para envio de mensagens e arquivos entre nós da rede"""
+
+    def __init__ (self, ip_destino, porta_destino, estacao, opcao, *cabecalho):
+        """Construtor da classe EnviaMensagem
+
+        Parâmetros de entrada:
+        ip_destino -- IP destino da mensagem
+        porta_destino -- porta destino da mensagem
+        estacao -- tipo de estação destino ('servidor', 'replica',
+        'cliente')
+        opcao -- tipo de operação a ser realizada
+        cabecalho (opcional) -- resto do cabeçalho da mensagem
+
+        """
+
+        # Inicia a classe thread
+        Thread.__init__(self)
+
+        # Criando cabeçalho padrão
+        header = '<div>' + str(estacao) + '#' +  str(opcao) + '#'
+
+        # Adicionando o resto do cabeçalho
+        for i in cabecalho[:-1]:
+            header = header + i + '#'
+        # Último item da lista não adiciona '#'
+        header = header + cabecalho[-1] + '<div>'
+
+        # Se o nome do arquivo for passado ele é o primeiro item do
+        # cabeçalho
+        try:
+            self.nome_arquivo = cabecalho[0]
+        except IndexError:
+            pass
+
+        # Inicializando variáveis da classe
+        self.header = header
+        self.tamanho_header = str(len(header)).zfill(3)
+        try:
+            self.ip_destino = str(ip_destino)
+            self.porta_destino = int(porta_destino)
+        except ValueError:
+            sys.exit('Valor de entrada inválida')
+
+    def run(self):
+        """Envia a mensagem e o arquivo"""
+
+        # Cria o socket
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            dest = (self.ip_destino, self.porta_destino)
+            sock.connect(dest)
+        except IOError:
+            err = "Falha ao criar o socket para o IP " + self.ip_destino + \
+            " na porta " + self.porta_destino
+            sys.exit(err)
+
+        # Envia o cabeçalho e seu tamanho
+        try:
+            sock.send(tamanho_header)
+            sock.send(header)
+        except:
+            err = "Falha ao enviar o cabeçalho da mensagem: " + header
+            sys.exit(err)
+
+        # Abre o arquivo caso ele exista
+        try:
+            caminho = os.path.abspath(self.nome_arquivo)
+            nome = os.path.basename(caminho)
+            arquivo = open(caminho).read()
+            tamanho_arquivo = len(arquivo)
+
+            # E envia o arquivo
+            totalsent = 0
+            while(totalsent < len(arquivo)):
+                sent = sock.send(arquivo[totalsent:])
+                if sent == 0:
+                    err = 'Não conseguiu enviar o arquivo'
+                    sys.exit(err)
+                totalsent = totalsent + sent
+
+        except IOError:
+            pass
+
+        # Fecha o socket e retorna que tudo deu certo
+        sock.close()
+        return 0
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
