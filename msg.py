34b0b2187db5446fe4ecfdd39e6fd71d8539e09a
Leandro M Barbosa <lbarbosa@linux.com>
2013-09-27 16:31:10 -0300

    Adiciona módulo para envio de mensagens.

--- msg.py
+++ msg.py
@@ -3,6 +3,12 @@
 
 """Provê funções para comunicação entre as partes do sistema"""
 
+import os
+import sys
+import socket
+import socketserver
+from threading import Thread
+
 # Compatibility between python2 and python3
 try:
     import cPickle
@@ -18,10 +24,22 @@ class Tipos:
     EXCECAO = 0x0008
     ERRO = 0x0016
 
+    strtipos_dict = {ENVIA_ARQUIVO: "Envia um arquivo ao servidor.",
+                     LISTA_ARQUIVOS: "Lista arquivos disponíveis.",
+                     REQUISITA_ARQUIVO: "Requista download de arquivo.",
+                     EXCECAO: "Exceção ocorreu.",
+                     ERRO: "Erro ocorreu."}
+
+    def strtipos(tipo):
+        try:
+            return Tipos.strtipos_dict[tipo]
+        except KeyError:
+            return "Tipo de mensagem desconhecido."
+
 class Erros:
     EDESC = 0x0001
 
-    strerro_dict = {EDESC: "Tipo de mensagem desconhecida."}
+    strerro_dict = {EDESC: "Tipo de mensagem desconhecido."}
 
     def strerro(erro):
         try:
@@ -64,3 +82,86 @@ def decodifica(mensagem):
 
     return pickle.loads(mensagem)
 
+
+class RecebeHandler(socketserver.BaseRequestHandler):
+    """Classe que executada para cada requisição"""
+
+    def handle(self):
+        """Função a ser executada para cada requisição"""
+
+        data = decodifica(self.request.recv(1024))
+
+        if sys.flags.debug:
+            print("Received data: {}".format(data))
+
+        if data[0] == Tipos.LISTA_ARQUIVOS:
+            resp_tipo = data[0]
+            resp_dados = ",".join(
+                [f for f in os.listdir('.') if os.path.isfile(f)])
+
+        resposta = codifica(resp_tipo, resp_dados)
+        self.request.sendall(resposta)
+
+class Servidor(socketserver.ThreadingMixIn,
+               socketserver.TCPServer):
+    """Classe que determinar o tipo do servidor"""
+    pass
+
+class Recebe:
+    """Classe para recebimento de mensagens e arquivos entre nós da rede
+
+    Variáveis da classe:
+    host -- endereço ip ou hostname para escuta
+    port_escuta -- porta de escuta para recebimento de conexões
+
+    """
+
+    def __init__(self, host=None, porta_escuta=5500):
+        if not host:
+            host = socket.gethostname()
+
+        servidor = Servidor((host, porta_escuta),
+                            RecebeHandler)
+        ip, porta = servidor.server_address
+        if sys.flags.debug:
+            print("Escutando em {}:{}".format(ip, porta))
+        # thread do servidor
+        t_servidor = Thread(target=servidor.serve_forever)
+        t_servidor.daemon = True
+        t_servidor.start()
+
+class Envia():
+    """Classe para envio de mensagens entre nós da rede"""
+
+    def __init__ (self, ip_destino=None, porta_destino=5500):
+        """Construtor da classe Envia
+
+        Variáveis:
+        ip_destino -- IP destino da mensagem
+        porta_destino -- porta destino da mensagem
+
+        """
+
+        if not ip_destino:
+            ip_destino = socket.gethostname()
+
+        try:
+            self.sock = socket.socket(socket.AF_INET,
+                                      socket.SOCK_STREAM)
+            dest = (ip_destino, porta_destino)
+            self.sock.connect(dest)
+        except IOError:
+            err = ("Falha ao criar o socket para o IP " +
+                   ip_destino +
+                   " na porta " + porta_destino)
+            sys.exit(err)
+
+    def envia(self, tipo, dados):
+        self.sock.sendall(codifica(tipo, dados))
+        resposta = decodifica(self.sock.recv(1024))
+
+        if sys.flags.debug:
+            print(resposta)
+
+    def close(self):
+        self.sock.close()
