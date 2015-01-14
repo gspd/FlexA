eca32927279580785c48561dc5d717b4d40aec5d
Mario Camara Neto <mariocamaraneto@gmail.com>
2015-01-14 21:14:12 -0200

    change client to work with Server.negotiate_store_part

--- client/flexa.py
+++ client/flexa.py
@@ -3,7 +3,7 @@ Created on 15/12/2014
 
 @author: mario
 '''
-#import class that set every configs
+# import class that set every configs
 from client.config import Config
 import crypto
 import sys
@@ -17,7 +17,7 @@ class Client():
     classdocs
     '''
     
-    #object that have every configs and parser
+    # object that have every configs and parser
     configs = None
     
     rpc = rpc_client.RPC()
@@ -31,89 +31,101 @@ class Client():
         
         args = Config.args
 
-        #Generate a new user key
+        # Generate a new user key
         if args.newkey:
-            #Checks if the user already has a key
+            # Checks if the user already has a key
             if os.path.exists(self.configs.get('User', 'private key')):
                 confirm = misc.query_yes_no("There is already a generated key, "
                         "generate another one?", default='no')
                 if not confirm:
                     sys.exit(2)
     
-            #createNewUserKey(self.configs)
+            # createNewUserKey(self.configs)
     
-        #Send a file to server
+        # Send a file to server
         if args.put:
             for names in args.put:
                 self.send_file(names)
     
-        #Get a file from server
+        # Get a file from server
         if args.get:
             for names in args.get:
                 self.recive_file(names)
     
         if args.list:
             self.list_files()
-        #Write configuration file
+        # Write configuration file
         with open(self.configs._config_path, mode='w', encoding='utf-8') as outfile:
             self.configs.loaded_config.write(outfile)
-        
-        
-        
-        
+
+
+
+
         
     def send_file(self, file_name):
         """
         send file from client to server
         """
-        
-        #name with your relative position (directory) in flexa system     
-        file_name_complete_relative = self.configs._dir_current_relative + file_name
-    
-        local_file = self.configs._dir_called + "/" + file_name
-        file_name_enc = file_name+".enc"
-        local_file_enc = self.configs._flexa_dir + file_name_enc
-    
-        server, ip_server = self.rpc.rpc_server()
-        print(self.configs.loaded_config.get("User","private key"))
-        rsa = crypto.open_rsa_key(self.configs.loaded_config.get("User","private key"),)
-    
-        user_id = self.configs.loaded_config.get("User","hash client")
-        #verify if this file exist (same name in this directory)
-        dir_key = "/" #FIXME set where is.... need more discussion
-        #ask to server if is update or new file
-        salt = server.get_salt(file_name_complete_relative, user_id)
-    
-        #generate every keys in string return tuple:
-        #(0 - verify, 1 - write, 2 - read, 3 - salt)
-        keys = crypto.keys_string(salt, rsa)
-        try:
-            print(local_file)
-            f = open(local_file, "rb") #verify if exist file
-            crypto.encrypt_file(keys[0][0:32], local_file, local_file_enc, 16)
-            f = open(local_file_enc, "rb") #verify if create file crypted
-            f.close()
-        except FileNotFoundError:
-            sys.exit("File not found.\nTry again.")
-    
-        type_file = "f"
-    
-        #if salt has a value then is update. because server return a valid salt
-        if salt:
-            port = server.update_file(keys[0], keys[1])
-        else:
-            #server return port where will wait a file
-            port = server.get_file(file_name_complete_relative, keys, dir_key, user_id, type_file)
-    
-        if not port:
-            sys.exit("Some error occurred. Maybe you don't have permission to \
-                    write. \nTry again.")
-    
-        host = (ip_server, port)
-        misc.send_file(host, local_file_enc)
-        #remove temp crypt file
-        os.remove(local_file_enc)
 
+        # name with your relative position (directory) in flexa system     
+        file_name_complete_relative = self.configs._dir_current_relative + file_name
+
+        local_file = self.configs._dir_called + "/" + file_name
+        file_name_enc = file_name + ".enc"
+        local_file_enc = self.configs._flexa_dir + file_name_enc
+
+        # verify if exist file
+        if not os.path.exists(local_file):
+            sys.exit("File not found.\nTry again.")
+
+        rsa = crypto.open_rsa_key(self.configs.loaded_config.get("User", "private key"),)
+
+        user_id = self.configs.loaded_config.get("User", "hash client")
+        # verify if this file exist (same name in this directory)
+        dir_key = "/"  # FIXME set where is.... need more discussion
+
+        total_parts_file = 1
+
+        server, ip_server = self.rpc.rpc_server()
+        # ask to server if is update or new file
+        salt = server.get_salt(file_name_complete_relative, user_id)
+
+        # generate every keys in string return vector:
+        # [0 - verify, 1 - write, 2 - read, 3 - salt]
+        keys = crypto.keys_string(salt, rsa)
+
+        crypto.encrypt_file(keys[2][0:32], local_file, local_file_enc, 16)
+
+        # verify if exist file
+        if not (misc.split_file(local_file_enc, total_parts_file)):
+            sys.exit("Problems with split file occurred.\nTry again.")
+
+        type_file = "f"
+
+        list_ports = []
+        # if salt has a value then is update. because server return a valid salt
+        if salt:
+            for num_part in range(total_parts_file):
+                list_ports.append(server.update_file(keys[0], keys[1], num_part))
+        else:
+            # server return port where will wait a file
+            keys[2] = 0
+            for num_part in range(total_parts_file):
+                port_on_server = server.negotiate_store_part(user_id, file_name_complete_relative, keys[0], dir_key, keys[1], keys[3], num_part)
+                list_ports.append(port_on_server)
+                if not port_on_server:
+                    sys.exit("Some error occurred. Maybe you don't have permission to \
+                            write. \nTry again.")
+
+        for num_part in range(total_parts_file):
+            host = (ip_server, list_ports[num_part - 1])
+            file_name_to_get = local_file_enc + '.' + str(num_part)
+            # TODO: colocar um if no send_file para confirmar se o envio foi efetuado com sucesso
+            misc.send_file(host, file_name_to_get)
+            os.remove(file_name_to_get)
+
+        # remove temp crypt file
+        os.remove(local_file_enc)
 
 
     def recive_file(self, file_name):
@@ -121,7 +133,7 @@ class Client():
         recive file from server
         """
     
-        #name with your relative position (directory) in flexa system 
+        # name with your relative position (directory) in flexa system 
         dir_file_relative = self.configs._dir_current_relative + file_name
         dir_file_local = self.configs._dir_called + '/' + file_name
         file_name_enc = file_name + '.enc'
@@ -129,11 +141,11 @@ class Client():
     
         ip = misc.my_ip()
         port, sock = misc.port_using(4001)
-        #make a thread that will recive file in socket
-        thr = Thread(target = misc.receive_file, args = (sock, dir_file_local_enc))
+        # make a thread that will recive file in socket
+        thr = Thread(target=misc.receive_file, args=(sock, dir_file_local_enc))
     
         server, ip_server = self.rpc.rpc_server()
-        user_id = self.configs.loaded_config.get("User","hash client")
+        user_id = self.configs.loaded_config.get("User", "hash client")
         salt = server.get_salt(dir_file_relative, user_id)
     
         if (salt == 0):
@@ -144,15 +156,17 @@ class Client():
         keys = crypto.keys_string(salt, rsa)
     
         thr.start()
-        #ask to server a file with name (keys[0] = hash)
-        #client ip and your port to recive file
-        if ( server.give_file(ip, port, keys[0]) ):
-            #exit with error and kill thread thr
+        
+        num_part = 0  # FIXME: colocar para descobrir automaticamante numero de partes
+        # ask to server a file with name (keys[0] = hash)
+        # client ip and your port to recive file
+        if (server.give_file(ip, port, keys[0], num_part)):
+            # exit with error and kill thread thr
             sys.exit("Some error occurs. Try again later.")
         thr.join()
     
-        crypto.decrypt_file(keys[0][0:32], file_name_enc, dir_file_local ,16)
-        #remove temp crypt file
+        crypto.decrypt_file(keys[2][0:32], file_name_enc, dir_file_local , 16)
+        # remove temp crypt file
         os.remove(dir_file_local_enc)
 
     def list_files(self):
