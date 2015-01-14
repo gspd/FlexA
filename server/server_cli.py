03f9f4fcb3418a0be3a828e57adb6db6e68bb52a
Mario Camara Neto <mariocamaraneto@gmail.com>
2015-01-14 20:59:12 -0200

    implements negotiate_store_part that do same thing that get_part did, change tha name of file now use name_file+"."+num_part, put logger in funtions, change function still_alive to get_server_status that will return a vector with your indeces of status (mem, cpu, network)

--- server/server_cli.py
+++ server/server_cli.py
@@ -19,13 +19,13 @@ from server.config import configs
 
 class Server(object):
     """Class that make rpc server
-    
+
         your constructor make configs to start server
-        
+
         functions:
             register_operations
                 register all functions that client can call by rpc
-            
+
     """
 
 
@@ -41,7 +41,7 @@ class Server(object):
         server = RPCThreadingServer(connection, requestHandler=RPCServerHandler)
         ip, port = server.server_address
         # Create local logging object
-        self.logger = logging.getLogger("init_server")
+        self.logger = logging.getLogger("Server_cli")
         self.logger.info("Listening on {}:{}".format(ip, port))
         # register all functions
         self.register_operations(server)
@@ -50,9 +50,9 @@ class Server(object):
             scanner = misc.Ping("255.255.255.255")
             scanner.daemon()
             server.serve_forever()
-        except KeyboardInterrupt as error:
+        except KeyboardInterrupt:
             print("\nSignal of interrupt received.")
-        except :
+        except:
             print("\nSomething made init_server stop.")
         finally:
             server.shutdown()
@@ -63,11 +63,11 @@ class Server(object):
         objects
         """
         server.register_function(self.list_files)
-        server.register_function(self.still_alive)
         server.register_function(self.give_file)
         server.register_function(self.get_file)
         server.register_function(self.get_salt)
         server.register_function(self.update_file)
+        server.register_function(self.negotiate_store_part)
 
     def list_files(self, verify_key):
         """
@@ -75,65 +75,121 @@ class Server(object):
                 verify_key - verify_key of current directory
         """
 
+        self.logger.info("list_files invoked")
+
         files_db = self.db.list_files(verify_key)
+
         list_file = []
         for file_obj in files_db:
             list_file.append(file.File(file_db = file_obj ))
 
         return list_file
 
-    def still_alive(self):
-        return 1
 
-    def give_file(self, ip, port, verify_key):
+    def get_server_status(self):
+        """
+            Analyze status of server with:
+                - how many disc is free
+                - how many connections is alive
+                - how many memory is in using
+            return a array of indices 
+        """
+
+        self.logger.info("get_server_status invoked")
+
+        return [1]
+
+
+    def give_file(self, ip, port, verify_key, num_part):
         """ give file to client
             ip: string with ip, address of client
             file_name: in a future this is a hash of file
         """
+
+        self.logger.info("give_file invoked")
+
         host = (ip, port)
-        misc.send_file(host, configs._dir_file + verify_key)
+        file_name_part = configs._dir_file + verify_key + '.' + str(num_part)
+        misc.send_file(host, file_name_part)
         #FIXME every rpc call return something - put sent confirmation
         return 0
 
 
-    def get_file(self, file_name, keys, dir_key, user_id, type_file):
+    def get_file(self, file_name, keys, dir_key, user_id, type_file, num_part):
         """get file from client
            file_name: name of file that will save in init_server - verify_key
-           keys: tupĺe (0 verify_key, 1 write_key, 2 read_key, 3 salt) strings
+           keys: tupĺe (0 verify_key, 1 write_key, 2 read_key (None), 3 salt) strings
         """
 
-        new_file = database.File(keys[0], keys[3], keys[1], keys[2], file_name, dir_key, user_id, type_file)
+        self.logger.info("get_file invoked")
+
+        new_file = database.File(keys[0], keys[3], keys[1], file_name, dir_key, user_id, type_file, num_part)
         self.db.add(new_file)
 
         #get a unusage port and mount a socket
         port, sockt = misc.port_using(5001)
 
-        print('name do arquivo: {}'.format(file_name), flush = True)
-        thread = Thread(target = misc.receive_file, args = (sockt, configs._dir_file + keys[0]))
+        file_name_to_get = configs._dir_file + keys[0] + '.' + str(num_part)
+        thread = Thread(target = misc.receive_file, args = (sockt, file_name_to_get))
         thread.start()
         #TODO: set timout to thread
 
         return port
 
-    def update_file(self, verify_key, write_key):
-        """get file from client
-           keys: tupĺe (0 verify_key, 1 read_key, 2 write_key, 3 salt) strings
+
+
+    def update_file(self, verify_key, write_key, num_part):
         """
+            if exist file, and client wanna send the same file (reference in db)
+            the server update file in system
+        """
+
+        self.logger.info("update_file invoked")
+
         #get a unusage port and mount a socket
         port, sockt = misc.port_using(5001)
 
         if not (self.db.update_file(verify_key, write_key)):
             return False
 
-        thread = Thread(target = misc.receive_file, args = (sockt, configs._dir_file + verify_key))
+        file_name_to_save = configs._dir_file + verify_key + '.' + str(num_part)
+        thread = Thread(target = misc.receive_file, args = (sockt, file_name_to_save ))
         thread.start()
         #TODO: set timout to thread
 
         return port
 
+
+
     def get_salt(self, file_name, user_id):
         """make a call in data base to find file
-            if found return your salt
+            if exist file return your salt
             else return 0
         """ 
+
+        self.logger.info("get_salt invoked")
+
         return self.db.salt_file(file_name, user_id)
+
+
+
+    def negotiate_store_part(self, user_id, file_name, verify_key, directory_key, write_key, salt, part_number):
+        """
+            Negotiate with client to server receive file part
+            (0 verify_key, 1 write_key, 2 read_key (None), 3 salt)
+        """
+
+        self.logger.info("negotiate_store_part invoked")
+
+        new_file = database.File(verify_key, salt, write_key, file_name, directory_key, user_id, part_number)
+        self.db.add(new_file)
+
+        #get a unusage port and mount a socket
+        port, sockt = misc.port_using(5001)
+
+        file_name_to_get = configs._dir_file + verify_key + '.' + str(part_number)
+        thread = Thread(target = misc.receive_file, args = (sockt, file_name_to_get))
+        thread.start()
+
+        return port
+        #TODO: set timout to thread
