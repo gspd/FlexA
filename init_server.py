c00e21ac81a793055d53d77e879392a53be30c0a
Mario Camara Neto <mariocamaraneto@gmail.com>
2015-06-27 21:48:17 -0300

    create class server to control client server and sync server

--- init_server.py
+++ init_server.py
@@ -4,25 +4,9 @@
 Servers is implemented in package server
 """
 
-from server.server_cli import Server
-
-
-def main():
-    """
-    The function called when the program is executed on a shell
-    """
-
-    #FIXME interface da rede
-    #broadcast = '192.168.1.255'
-    #from server.config import configs
-    #connection = (ip, configs._port_sync)
-    #th = Thread(target = server.server_sync.Sync, args = (connection, broadcast), daemon = True)
-    #th.start()
-
-    #Start server_cli
-    Server()
-
+from server import server
 
 if __name__ == '__main__':
-    main()
+    server.Server()
+
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
---
+++ server/server.py
@@ -0,0 +1,20 @@
+'''
+Created on 28/06/2015
+
+@author: mario
+'''
+
+import server
+
+class Server():   
+    '''
+    Initialize Synchronism Server and Client Server
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+        server.sync_server.Sync_Server()
