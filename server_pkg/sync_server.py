fcd249142ed8749df7817bdf0b930ef8c5bbaa08
Mario Camara Neto <mariocamaraneto@gmail.com>
2015-07-20 20:12:55 -0300

    start to find your neighbor

--- server_pkg/sync_server.py
+++ server_pkg/sync_server.py
@@ -7,9 +7,10 @@ Created on 23/11/2014
 from server_pkg.server import Server
 from rpc import RPCThreadingServer
 from rpc import RPCServerHandler
-import logging
-
 from multiprocessing import Process
+from server_pkg.RPC import RPC
+import logging
+from time import sleep
 
 class Sync_Server(Process, Server):
     
@@ -34,8 +35,12 @@ class Sync_Server(Process, Server):
         self.logger = logging.getLogger("sync_server")
         self.logger.info("Listening on {}:{}".format(ip, port))
 
+        self.server_obj = RPC()
+
         # register all functions
         self.register_operations(server)
+        Process(target = self.scan_neighbor, daemon = True).start()
+
         # create and init_server object
         try:
             server.serve_forever()
@@ -47,11 +52,17 @@ class Sync_Server(Process, Server):
         server.register_function(self.get_neighbor_map)
         server.register_function(self.update_neighbor)
 
-
     def get_neighbor_map(self):
-
-        return self.left_neighbor+self.right_neighbor
+        return (self.left_neighbor+[Server.configs.uid]+self.right_neighbor)
 
     def update_neighbor(self):
-
         pass
+
+    def scan_neighbor(self):
+        while True:
+            server_conn = self.server_obj.get_next_server_not_me()
+            window = server_conn.get_neighbor_map()
+            print("A janela recebida é",window)
+            sleep(10)
+
+
