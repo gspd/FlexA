d6ef9cc58fd71b5ad4b23293bab71b1fc0beecc7
Mario Camara Neto <mariocamaraneto@gmail.com>
2015-07-27 23:04:26 -0300

    call every process
    Init_server is responsable to set and initialize all process necessary to work well.
    What it do:
        -set configs of server
        -instantiation process
        -start process
        -join process (exception daemon)

--- init_server.py
+++ init_server.py
@@ -4,13 +4,28 @@
 Servers is implemented in package server
 """
 
-from server_pkg import server
+from server_pkg import sync_server, cli_server, neighbor, server
+import misc
 
 if __name__ == '__main__':
+    
+    server = server.Server()
 
-    local_server = server.Server()
+    #local network machines finder
+    scanner = misc.Ping("255.255.255.255") 
+    neighbor = neighbor.Neighbor(server)
+    sync = sync_server.Sync_Server(server=server, neighbor=neighbor)
+    cli = cli_server.Client_Server(server=server, neighbor=neighbor)
 
-    local_server.start_services()
+    scanner.start() #daemon
+    sync.start() #daemon
+    neighbor.start()
+    cli.start()
 
+    try:
+        sync.join()
+        cli.join()
+    except:
+        exit(0)
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
